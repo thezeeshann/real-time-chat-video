90ca7fa33ca7223047ce5ff23ea96ad92adc78d8
thezeeshann <info.zeeshann@gmail.com>
2024-08-02 23:24:41 +0530

    feat: Added socket server

--- client/src/communication/socket.js
+++ client/src/communication/socket.js
@@ -2,9 +2,12 @@ import io from "socket.io-client";
 
 let socket = null;
 
-export const connectWithSocketServer = () => {
-  socket = io("http://localhost:8000");
-
+export const connectWithSocketServer = (userToken) => {
+  socket = io("http://localhost:8000", {
+    auth: {
+      token: userToken,
+    },
+  });
   socket.on("connect", () => {
     console.log("successfully connect with socket.io server");
     console.log(socket.id);
