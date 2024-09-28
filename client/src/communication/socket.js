78ce46dcb80b44d1e979051c014bc261a13245c4
thezeeshann <info.zeeshann@gmail.com>
2024-09-28 23:14:29 +0530

    refactor: updated client socket

--- client/src/communication/socket.js
+++ client/src/communication/socket.js
@@ -1,8 +1,9 @@
 import io from "socket.io-client";
+import { pendingFriendsInvitations } from "../redux/features/friendSlice";
 
 let socket = null;
 
-export const connectWithSocketServer = (userToken) => {
+export const connectWithSocketServer = (userToken, dispatch) => {
   socket = io("http://localhost:8000", {
     auth: {
       token: userToken,
@@ -12,4 +13,11 @@ export const connectWithSocketServer = (userToken) => {
     console.log("successfully connect with socket.io server");
     console.log(socket.id);
   });
+
+  socket.on("friend-invitation", (data) => { 
+    const { pendingInvitations } = data;
+    console.log("friend invitation event received", pendingInvitations);
+    dispatch(pendingFriendsInvitations(pendingInvitations));
+  });
+  
 };
