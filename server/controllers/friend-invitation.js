12484b39cda806bce8c0745a2a5a0ddc3cacb87e
thezeeshann <info.zeeshann@gmail.com>
2024-09-29 23:09:16 +0530

    refactor: update friend controllers & routes

--- server/controllers/friend-invitation.js
+++ server/controllers/friend-invitation.js
@@ -1,4 +1,5 @@
 import friendInvitationSchema from "../lib/validations/friend.js";
+import friendInvitationDecisionSchema from "../lib/validations/friendInvitations.js";
 import FriendModel from "../models/friend.js";
 import UserModel from "../models/user.js";
 import friendsPendingInvitations from "../socketHandler/updates/friends.js";
@@ -55,12 +56,84 @@ export const sendFriendInvitation = async (req, res) => {
       receiverId: targetUser._id,
     });
 
-    friendsPendingInvitations(targetUser._id.toString())  
+    friendsPendingInvitations(targetUser._id.toString());
 
     return res.status(201).json({
       success: true,
       message: "Invitation has been send",
       newInvitation,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const acceptFriendInvitation = async (req, res) => {
+  try {
+    const body = await friendInvitationDecisionSchema.validate(req.body);
+    const { id } = body;
+
+    const invitation = await FriendModel.findById(id);
+    if (invitation) {
+      return res.status(401).json({
+        success: false,
+        message: "Something went wrong. please try again",
+      });
+    }
+
+    const { receiverId, senderId } = invitation;
+
+    // add friends to both users
+    const senderUser = await UserModel.findById(senderId);
+    senderUser.friends = [...senderUser.friends, receiverId];
+
+    const receiverUser = await UserModel.findById(receiverId);
+    receiverUser.friends = [...receiverUser.friends, senderId];
+
+    await senderUser.save();
+    await receiverUser.save();
+
+    // delete invitations
+    await FriendModel.findOneAndDelete(id);
+
+    // update list of friends pending invitations
+    friendsPendingInvitations(receiverId);
+
+    return res.status(201).json({
+      success: true,
+      message: "Friend Successfully added",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const rejectFriendInvitation = async (req, res) => {
+  try {
+    const body = await friendInvitationDecisionSchema.validate(req.body);
+    const { id } = body.value;
+    const { userId } = req.existUser;
+    const existInvitation = await FriendModel.findById(id);
+
+    if (!existInvitation) {
+      return res.status(404).json({
+        success: false,
+        message: "Invitation not found",
+      });
+    }
+
+    await FriendModel.findByIdAndDelete(id);
+    friendsPendingInvitations(userId);
+
+    return res.status(201).json({
+      success: true,
+      message: "Invitation has been rejected",
     });
   } catch (error) {
     return res.status(500).json({
--- server/routes/friends.js
+++ server/routes/friends.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { verifyJwt } from "../lib/middleware/auth.js";
-import { sendFriendInvitation } from "../controllers/friend-invitation.js";
+import {
+  sendFriendInvitation,
+  acceptFriendInvitation,
+  rejectFriendInvitation,
+} from "../controllers/friend-invitation.js";
 
 const router = express.Router();
 
 router.post("/invitation", verifyJwt, sendFriendInvitation);
+router.post("/accept", verifyJwt, acceptFriendInvitation);
+router.post("/reject", verifyJwt, rejectFriendInvitation);
 
 export default router;
