3c3411ab0c81d3b4176765d418bcad853d6d3f20
thezeeshann <info.zeeshann@gmail.com>
2024-09-29 23:09:47 +0530

    refactor: Added accept & reject api endpoint

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -3,3 +3,5 @@ const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 export const SIGNUP_API = BASE_URL + "auth/register";
 export const SIGNIN_API = BASE_URL + "auth/login";
 export const FRIEND_INVITATION_API = BASE_URL + "friend/invitation";
+export const FRIEND_INVITATION_ACCEPT_API = BASE_URL + "friend/accept";
+export const FRIEND_INVITATION_REJECT_API = BASE_URL + "friend/reject";
