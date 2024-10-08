29815fff14e5f4f26ccb63ad31dce180c5bbed4d
thezeeshann <info.zeeshann@gmail.com>
2024-10-08 22:44:18 +0530

    refactor: update chat history handler

--- server/socketHandler/directChatHistoryHandler.js
+++ server/socketHandler/directChatHistoryHandler.js
@@ -1,16 +1,12 @@
-import { updateChatHistory } from "./updates/chat.js";
 import ConversationModel from "../models/conversation.js";
 
 export const directChatHistoryHandler = async (socket, data) => {
   try {
     const { userId } = socket.user;
     const { receiverUserId } = data;
-
     const conversation = await ConversationModel.findOne({
-      participants: {
-        $all: [userId, receiverUserId],
-        type: "DIRECT",
-      },
+      participants: { $all: [userId, receiverUserId] },
+      type: "DIRECT",
     });
 
     if (conversation) {
