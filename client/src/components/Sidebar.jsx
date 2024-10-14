50b81787a7e56513feecce19c9a300ef5bb1e746
thezeeshann <info.zeeshann@gmail.com>
2024-10-14 21:09:18 +0530

    refactor: update components

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,15 +4,16 @@ import { FRIEND_INVITATION_API } from "../redux/api";
 import { GoDotFill } from "react-icons/go";
 import toast from "react-hot-toast";
 import { useSelector } from "react-redux";
+import { IoMdAdd } from "react-icons/io";
 import {
   handleAcceptInvitations,
   handleRejectInvitations,
 } from "../utils/friendInvitation";
+import { IoPersonSharp } from "react-icons/io5";
 import { setChosenChatDetails } from "../redux/features/chatSlice";
 import { useDispatch } from "react-redux";
 import { chatTypes } from "../redux/features/chatSlice";
-// import { FaRegSquarePlus } from "react-icons/fa6";
-import { createNewRoom } from "../communication/roomHandler";
+// import { createNewRoom } from "../communication/roomHandler";
 
 const Sidebar = () => {
   const dispatch = useDispatch();
@@ -56,95 +57,98 @@ const Sidebar = () => {
       })
     );
   };
-  const createRoomButton = () => {
-    createNewRoom(dispatch);
-  };
+  // const createRoomButton = () => {
+  //   createNewRoom(dispatch);
+  // };
 
   return (
     <>
-      <div className="relative flex flex-col w-64 min-h-screen text-white bg-gray-800">
-        <div className="flex items-center justify-center w-full">
-          <button
-            onClick={createRoomButton}
-            className="text-white bg-blue-500 rounded-md w-[20%] cursor-pointer py-2 font-semibold"
-          >
-            {/* <FaRegSquarePlus size={22} color="#ffffff" className="" /> */}
-            <p>Room</p>
-          </button>
-          <h1
-            className="p-2 w-[60%] font-semibold text-center bg-green-500 rounded-md cursor-pointer"
-            onClick={() => setIsOpen((prev) => !prev)}
-          >
-            Add Friend
-          </h1>
+      <div className=" relative flex flex-row w-[300px] min-h-screen">
+        <div className=" w-[18%] text-white bg-gray-900 space-y-1">
+          <div className="flex items-center justify-center w-10 h-10 p-1 mx-auto bg-blue-500 rounded-xl">
+            <IoPersonSharp  color="#ffffff" size={22} />
+          </div>
+          <div  className="flex items-center justify-center w-10 h-10 p-1 mx-auto bg-blue-500 cursor-pointer rounded-xl">
+            <IoMdAdd color="#ffffff" size={22} />
+          </div>
         </div>
-        {/* <p className="mt-2 font-semibold text-center">Private message</p> */}
-        <div className="flex-1 p-4">
-          {friends.map((friend) => {
-            const isOnline = onlineUsers.some(
-              (user) => user.socketId.userId === friend.id
-            );
-            return (
-              <div
-                key={friend.id}
-                className="flex flex-col py-1 cursor-pointer gap-y-1 "
-                onClick={() =>
-                  handleChooseActiveChat(dispatch, friend?.id, friend?.name)
-                }
-              >
-                <div className="flex flex-row items-center justify-between">
-                  <div className="flex flex-row items-center gap-x-2 ">
-                    <span
-                      className={`${
-                        chosenChatDetails?.id === friend?.id
-                          ? "bg-blue-500"
-                          : "bg-gray-500"
-                      } p-2 font-semibold text-white capitalize rounded-full`}
-                    >
-                      {friend?.name?.slice(0, 2)}
-                    </span>
-                    <p className="capitalize">{friend?.name}</p>
+
+        <div className=" flex flex-col w-[82%] min-h-screen text-white bg-gray-800 ">
+          <div className="flex items-center justify-center w-full">
+            <h1
+              className="w-full p-2 font-semibold text-center bg-green-500 rounded-md cursor-pointer"
+              onClick={() => setIsOpen((prev) => !prev)}
+            >
+              Add Friend
+            </h1>
+          </div>
+          <div className="flex-1 p-4">
+            {friends.map((friend) => {
+              const isOnline = onlineUsers.some(
+                (user) => user.socketId.userId === friend.id
+              );
+              return (
+                <div
+                  key={friend.id}
+                  className="flex flex-col py-1 cursor-pointer gap-y-1 "
+                  onClick={() =>
+                    handleChooseActiveChat(dispatch, friend?.id, friend?.name)
+                  }
+                >
+                  <div className="flex flex-row items-center justify-between">
+                    <div className="flex flex-row items-center gap-x-2 ">
+                      <span
+                        className={`${
+                          chosenChatDetails?.id === friend?.id
+                            ? "bg-blue-500"
+                            : "bg-gray-500"
+                        } p-2 font-semibold text-white capitalize rounded-full`}
+                      >
+                        {friend?.name?.slice(0, 2)}
+                      </span>
+                      <p className="capitalize">{friend?.name}</p>
+                    </div>
+                    {isOnline ? (
+                      <span>
+                        <GoDotFill size={20} className="text-green-500" />
+                      </span>
+                    ) : (
+                      <span>
+                        <GoDotFill size={20} className="text-gray-500" />
+                      </span>
+                    )}
                   </div>
-                  {isOnline ? (
-                    <span>
-                      <GoDotFill size={20} className="text-green-500" />
-                    </span>
-                  ) : (
-                    <span>
-                      <GoDotFill size={20} className="text-gray-500" />
-                    </span>
-                  )}
                 </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="flex flex-col p-4 gap-y-2">
-          <p className="w-full text-blue-500">INVITATIONS</p>
-          {pendingFriends.map((friend) => (
-            <div key={friend._id} className="flex flex-col gap-y-2">
-              <div className="flex flex-row items-center justify-between ">
-                <div className="flex flex-row items-center gap-x-2">
-                  <span className="p-2 capitalize bg-blue-500 rounded-full">
-                    {friend?.senderId?.name.slice(0, 2)}
+              );
+            })}
+          </div>
+          <div className="flex flex-col p-4 gap-y-2">
+            <p className="w-full text-blue-500">INVITATIONS</p>
+            {pendingFriends.map((friend) => (
+              <div key={friend._id} className="flex flex-col gap-y-2">
+                <div className="flex flex-row items-center justify-between ">
+                  <div className="flex flex-row items-center gap-x-2">
+                    <span className="p-2 capitalize bg-blue-500 rounded-full">
+                      {friend?.senderId?.name.slice(0, 2)}
+                    </span>
+                    <p className="capitalize">{friend?.senderId?.name}</p>
+                  </div>
+                  <span
+                    onClick={() => handleRejectInvitations(friend._id, token)}
+                    className="cursor-pointer"
+                  >
+                    X
                   </span>
-                  <p className="capitalize">{friend?.senderId?.name}</p>
+                  <span
+                    onClick={() => handleAcceptInvitations(friend._id, token)}
+                    className="cursor-pointer"
+                  >
+                    ✓
+                  </span>
                 </div>
-                <span
-                  onClick={() => handleRejectInvitations(friend._id, token)}
-                  className="cursor-pointer"
-                >
-                  X
-                </span>
-                <span
-                  onClick={() => handleAcceptInvitations(friend._id, token)}
-                  className="cursor-pointer"
-                >
-                  ✓
-                </span>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
 
